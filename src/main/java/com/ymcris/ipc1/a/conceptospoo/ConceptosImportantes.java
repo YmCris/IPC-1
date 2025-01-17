932accadeb412eca16ce403747fce3d83ed55daa
Cris <ymcrisystems@gmail.com>
2025-01-16 22:10:43 -0600

    Hola mundo

--- src/main/java/com/ymcris/ipc1/a/conceptospoo/ConceptosImportantes.java
+++ src/main/java/com/ymcris/ipc1/a/conceptospoo/ConceptosImportantes.java
@@ -5,5 +5,7 @@ package com.ymcris.ipc1.a.conceptospoo;
  * @author crist
  */
 public class ConceptosImportantes {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
